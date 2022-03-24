66ac90a0027ebbc6fee051e92cc906cbaad605a0
Jake McGee <jbm2015@gmail.com>
2022-03-23 20:47:26 -0400

    added comments to help ya'll get started, if you need it

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+# tkinter is already installed to pycharm. idk if pyclasp is too, but if it's not you can install it fairly easily
+# just hover over the red squiggly line and ask pycharm to install it, if you need to
 import tkinter
 import pyclasp as clasp
