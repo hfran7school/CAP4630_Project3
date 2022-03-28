be1b20730627a4fd525ea054cb4aa0348f87ced2
Jake McGee <jbm2015@gmail.com>
2022-03-28 17:51:38 -0400

    started createAllObjects, updated attributes.txt to reflect constraints as cnf

--- preferenceLogic.py
+++ preferenceLogic.py
@@ -1,20 +1,25 @@
-import pyclasp as clasp
+import subprocess as sp
 
 class Logic:
 
-    def penaltyLogic(self, obj, rule):
+    def penaltyLogic(self, rule):
         """Determines if an object conforms to a penalty logic rule. Returns 0 if it does, the penalty value otherwise."""
 
-    def probabilisticLogic(self, obj, rule):
+    def probabilisticLogic(self, rule):
         """Determines if an object conforms to a probabilistic logic rule. Returns 1 if it does, the probabilistic value
         otherwise."""
 
-    def qualitativeLogic(self, obj, rule):
+    def qualitativeLogic(self, rule):
         """Determines if an object conforms to a qualitative logic rule. Returns the number of the first rule the object
         conforms to, inf if no rules apply."""
 
-    def createFeasibleObjects(self, obj, constraint):
+    def createFeasibleObjects(self, constraint):
         """Returns a list of all feasible objects, represented in binary"""
 
     def createAllObjects(self, attr):
-        """Returns a list of all objects possible, not considering the constraints"""
+        """Returns a list of all objects possible, not considering the constraints\nattr : number of attributes"""
+        fp = open('allattr.cnf', 'w')
+        fp.write('p cnf ' + attr + ' 0')
+        fp.close()
+        claspOut = sp.run('clingo --mode=clasp allattr.cnf -n 0', shell=True, capture_output=True)
+        print(claspOut)
