c8dbe5d28901291f9583fa3edcc6b2707f000754
Jake McGee <jbm2015@gmail.com>
2022-03-31 18:44:57 -0400

    added exemplify, omni-optimize, and optimize functions

--- preferenceLogic.py
+++ preferenceLogic.py
@@ -1,5 +1,6 @@
 import subprocess as sp
 import os
+import random
 
 class Logic:
 
@@ -89,7 +90,7 @@ class Logic:
         return result
 
     def findIndex(self, list, search):
-        """Returns the index of a value in a list, or -1 if it can't be found"""
+        """Returns the index of a value in a list, or -1 if it can't be found41"""
         a = 0;
         for i in list:
             if i == search:
@@ -116,6 +117,76 @@ class Logic:
             return 0
         else:
             return 1
+
+    def exemplify(self, feasibleObjects: list, rules: tuple) -> list:
+        """
+        Generates all feasible objects, picks two at random, and compares them according to the different logics.
+        :param feasibleObjects: the list of feasible objects
+        :param rules: the preference rules to use. Must be a 3-tuple, with the elements being the rules for penalty, possibilistic, and qualitative logics, respectfully
+        :return: a list containing a 2-tuple with the objects used, and the number of the object (1 or 2) that is preferred for each of the logics, in the above order, with 0 being congruency (and -1 for incompatibility for qualitative)
+        """
+        # Select two random objects
+        results = []
+        obj1Int = random.randrange(0, len(feasibleObjects))
+        obj2Int = random.randrange(0, len(feasibleObjects))
+        if obj1Int == obj2Int:
+            if obj2Int+1 == len(feasibleObjects):
+                obj2Int -= 1
+            else:
+                obj2Int += 1
+        obj1 = feasibleObjects[obj1Int]
+        obj2 = feasibleObjects[obj2Int]
+        results.append((obj1, obj2))
+
+        # Test 1: Penalty logic. Ruleset = rules[0]
+        penaltyResults = self.determinePenaltyPreference([obj1, obj2], rules[0])
+        if len(penaltyResults) != 2:
+            results.append(0)
+        else:
+            if penaltyResults[0][0] == obj1:
+                results.append(1)
+            else:
+                results.append(2)
+
+        # Test 2: Possibilistic logic. Ruleset = rules[1]
+        possibilisticResults = self.determinePossibilisticPreference([obj1, obj2], rules[1])
+        if len(possibilisticResults) != 2:
+            results.append(0)
+        else:
+            if possibilisticResults[0][0] == obj1:
+                results.append(1)
+            else:
+                results.append(2)
+
+        # Test 3: Qualitative Choice logic. Ruleset = rules[2]
+        # TODO: this part
+
+        return results
+
+    def omniOptimize(self, feasibleObjects: list, rules: tuple):
+        """
+        Runs penalty, possibilistic, and qualitative logics on all feasible objects, and returns all optimal objects.
+        :param feasibleObjects: all feasible objects
+        :param rules: the preference rules to use. Must be a 3-tuple, with the elements being the rules for penalty, possibilistic, and qualitative logics, respectfully
+        :return: a list containing tuples of the optimal objects for each logic, in order (penalty, possibilistic, qualitative)
+        """
+        penaltyOptimal = self.determinePenaltyPreference(feasibleObjects, rules[0])
+        possibilisitcOptimal = self.determinePossibilisticPreference(feasibleObjects, rules[1])
+        # TODO: qualitativeOptimal = self.determineQualitativePreference(feasibleObjects, rules[2]
+        return [penaltyOptimal[0], possibilisitcOptimal[0]]
+
+    def optimize(self, feasibleObjects: list, rules: tuple):
+        """
+        Runs omniOptimize, selects one random element from each list, and returns each.
+        :param feasibleObjects: all feasible objects
+        :param rules: the preference rules to use. Must be a 3-tuple, with the elements being the rules for penalty, possibilistic, and qualitative logics, respectfully
+        :return: a list containing one optimal object for each of the logics
+        """
+        optimal = self.omniOptimize(feasibleObjects, rules)
+        rand1 = random.randrange(0, len(optimal[0]))
+        rand2 = random.randrange(0, len(optimal[1]))
+
+        return [optimal[0][rand1], optimal[1][rand2]]
 
     def createFeasibleObjects(self, constraints: str) -> list[str]:
         """Returns a list of all feasible objects. If no feasible objects are possible, return []\n
