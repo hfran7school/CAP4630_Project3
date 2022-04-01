7b381dec700a89041fa8d698f8708afe24ea1187
SquidH <98364301+SquidH@users.noreply.github.com>
2022-04-01 16:57:55 -0400

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -1,5 +1,16 @@
 class Inputs:
+    """
+        Doc
+    """
     def updateDictionary(self, attributes: list):
+        """
+        Reads through each item in the list, splitting at the comma and creating a dictionary for the integer value
+        of the attribute options before inverting the dictionary and returning a list containing both the default
+        and inversed dictionary.
+
+        :param attributes:  List of attributes which contain every option combination determined by the user.
+        :return:            List with first value being the cnfDictionary and the second value being the inverse.
+        """
         count = 1
         dictionary = {}
         for item in attributes:
@@ -9,10 +20,17 @@ class Inputs:
             count += 1
         revDictionary = {v: k for k, v in dictionary.items()}
         twoDictionaries = [dictionary, revDictionary]
-        return twoDictionaries  # Returns a list with first value being the cnfDictionary and the second value being the inverse
-
+        return twoDictionaries
 
     def cnfConstraints(self, constraints: list, cnfDict: dict):
+        """
+        Reads through each item in the list, creating a string in cnf form (as per the requirements for using clasp)
+        and returning it after reading through the list.
+
+        :param constraints: List of contraints which contain every clause for determining feasible objects.
+        :param cnfDict:     The cnfDictionary which contains the cnf values of each attribute option.
+        :return:            Returns a string which matches the file input for clasp.
+        """
         attr = cnfDict
         numattributes = int(len(attr) / 2)
         numclauses = 0
@@ -29,10 +47,7 @@ class Inputs:
                     item = int(attr[term]) * -1
                     textformat += str(item)
                     lastnot = False
-                elif term == "AND":  # The AND is implicit in hard constraints -- keeping regardless
-                    textformat += " 0\n"
-                    numclauses += 1
-                elif term == "OR":
+                elif term == "OR" or term == "AND":
                     textformat += " "
                 else:
                     textformat += str(attr[term])
@@ -41,8 +56,17 @@ class Inputs:
         cnfString = "p cnf " + str(numattributes) + " " + str(numclauses) + "\n" + textformat
         return cnfString
 
-# Used for both PENALTY and POSSIBILISTIC logics
     def cnfLogic(self, logicLines: list, cnfDict: dict):
+        """
+        Used for penalty and possibilistic logics, reads through each item in the list, splitting at the comma to save
+        the value of the condition and creates a string in cnf form. The string and value are paired in a tuple and
+        appended to a list, which is returned at the end of the function.
+
+        :param logicLines:  List of rules which contain a clause and value attached to that clause for either
+                            penalty or possibilistic logic rules.
+        :param cnfDict:     The cnfDictionary which contains the cnf values of each attribute option.
+        :return:            Returns list of tuples in format of [(cnfstring, value), ...]
+        """
         logic = cnfDict
         numattr = int(len(logic) / 2)
         numclauses = 1
@@ -72,10 +96,20 @@ class Inputs:
             logictuples.append(t)
             textformat = ""
             numclauses = 1
-        return logictuples  # Returns list of tuples in format of [(cnfstring, value), ...]
+        return logictuples
 
-# Used for QUALITATIVE logic
     def cnfQualitative(self, qualLines: list, cnfDict: dict):
+        """
+        Used for qualitative logic, reads through each item in the list, first checking to see if there is a condition
+        before saving it -- or 0 to mark no conditon -- and splits at the IF, reading through each item to determine the
+        cnf form string for each line and preparing a list of tuples which contain the condition and a nested tuple of
+        each rule matching the condition that it will return.
+
+        :param qualLines:   List of rules which contain a clause and (optionally) a value attached to that clause for
+                            qualitative logic rules.
+        :param cnfDict:     The cnfDictionary which contains the cnf values of each attribute option.
+        :return:            Returns list of tuples in format of [(condition, (cnf, ...)), ...]
+        """
         numattr = int(len(cnfDict) / 2)
         numclauses = 1
         textformat = ""
@@ -118,5 +152,4 @@ class Inputs:
             rule[:] = []
             textformat = ""
             numclauses = 1
-        return qualtuples  # Returns list of tuples in format of [(condition, (cnf, ...)), ...]
-        # Condition will be 0 if there are no conditions.
+        return qualtuples
